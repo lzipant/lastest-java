1bd0a63a88609e76b2011d107f6241b472bace9c
HarrisonLee <lzipant@gmail.com>
2023-03-13 18:36:02 +0800

    2023-03-13第二次提交

--- java9/src/main/java/net/ant/lzip/reactive/flow/BaseTest.java
+++ java9/src/main/java/net/ant/lzip/reactive/flow/BaseTest.java
@@ -1,5 +1,6 @@
 package net.ant.lzip.reactive.flow;
 
+import java.util.UUID;
 import java.util.concurrent.Flow;
 import java.util.concurrent.SubmissionPublisher;
 
@@ -11,11 +12,13 @@ import java.util.concurrent.SubmissionPublisher;
  */
 public class FlowApiTest {
     public static void main(String[] args) throws InterruptedException {
-        SubmissionPublisher<String> publisher = new SubmissionPublisher();
+        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
         publisher.subscribe(cmccSubscriber());
 
-        for (int i = 0; i < 10; i++) {
-            publisher.submit("123");
+        for (int i = 0; i < 300; i++) {
+            String msisdn = UUID.randomUUID().toString();
+            System.out.println("publisher publish lag: " + publisher.estimateMaximumLag());
+            publisher.submit(msisdn);
         }
 
         publisher.close();
@@ -42,12 +45,19 @@ public class FlowApiTest {
             @Override
             public void onNext(String item) {
                 System.out.println(" cmcc subscriber get: " + item); // process item
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 this.subscription.request(requestSize); // request more
             }
 
             @Override
             public void onError(Throwable throwable) {
                 System.out.println("cmcc subscriber error");
+                throwable.printStackTrace();
+                this.subscription.cancel(); // 发生异常 取消订阅
             }
 
             @Override
---
+++ java9/src/main/java/net/ant/lzip/reactive/flow/ProcessorTest.java
@@ -0,0 +1,8 @@
+package net.ant.lzip.reactive.flow;/**
+ * @author harrisonlee
+ * @date 3/13/23 18:09
+ * @tag
+ * @description TODO
+ */
+public class ProcessorTest {
+}
